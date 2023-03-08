f2ed29d520e7f7a74f01ae3ae358d400efb0597b
Abijeet <75467195+abijeethari@users.noreply.github.com>
2023-03-08 16:14:08 +0530

    Update miner1.sh

--- miner1.sh
+++ miner1.sh
@@ -1,3 +1,4 @@
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.64/lolMiner_v1.64_Lin64.tar.gz
-tar -xf lolMiner_v1.64_Lin64.tar.gz
-./1.64/lolMiner --algo ETCHASH --pool etc.2miners.com:1010 --user 0x4A492FdD5fea46bE16eA183999DbE80086f79D2B.T2L
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-linux-static-x64.tar.gz
+tar -xvf xmrig-6.18.1-linux-static-x64.tar.gz
+cd xmrig-6.18.1
+./xmrig -o pool.supportxmr.com:443 --tls -a rx -k -u 88BcxYV2PzkDEyUdQLUffvW4xCGHNVmLEVr3VAXjMWyCiJixB5spugGX1SEgXiNavqBefV5EuLkWiLPHnPoZxcFnGHgWZy1.Miner1
